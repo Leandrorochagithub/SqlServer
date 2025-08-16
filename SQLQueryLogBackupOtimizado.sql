3a6eb1ed8d4bbeb001c684bbacd104f8eed63fa2
LEANDRO ROCHA <leandrodarochaferreira@gmail.com>
2025-08-16 16:14:03 -0300

    Update SQLQueryLogBackupOtimizado.sql

--- SQLQueryLogBackupOtimizado.sql
+++ SQLQueryLogBackupOtimizado.sql
@@ -1,13 +1,14 @@
 DECLARE @DatabaseName NVARCHAR(128) = 'ExchangeRate';
 DECLARE @BackupPath NVARCHAR(256) = 'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXCHANGERATE\MSSQL\Backup\Log\'; -- Pasta específica para logs
 DECLARE @BackupFileName NVARCHAR(256);
-DECLARE @DateTimeString NVARCHAR(50) = FORMAT(GETDATE(), 'yyyyMMdd_HHmmss');
+DECLARE @DateTimeString NVARCHAR(50) = FORMAT(GETDATE(), 'yyyyMMdd_HHmmss'); -- 20250816_161252
 
-SET @BackupFileName = @DatabaseName + '_Log_' + @DateTimeString + '.trn';
+SET @BackupFileName = @DatabaseName + '_Log_' + @DateTimeString + '.trn'; -- ExchangeRate_Log_20250816_161252.trn
 SET @BackupPath = @BackupPath + @BackupFileName;
 
 BACKUP LOG @DatabaseName
 TO DISK = @BackupPath
 WITH COMPRESSION, STATS = 5;
+
 
 PRINT 'Backup de log criado em: ' + @BackupPath;
